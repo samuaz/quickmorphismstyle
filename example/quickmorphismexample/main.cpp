e0b9d995dbe29ac51a0164ed41d26000ee323032
Samuel Azcona <samuaz@gmail.com>
2022-11-13 03:19:31 -0300

    update

--- example/quickmorphismexample/main.cpp
+++ example/quickmorphismexample/main.cpp
@@ -6,9 +6,6 @@
 #include <QTranslator>
 #include <QtQml/qqmlextensionplugin.h>
 
-Q_IMPORT_QML_PLUGIN(QuickMorphismStylePlugin)
-
-
 //int main(int argc, char *argv[])
 //{
 //    QGuiApplication app(argc, argv);
@@ -55,8 +52,8 @@ int main(int argc, char *argv[])
     // QuickMorphism Init
     QuickMorphism::init(engine);
 
-    //const QUrl url(u"qrc:/quickmorphismexample/main.qml"_qs);    
-    const QUrl url(QStringLiteral("qrc:/Demo.qml"));
+    const QUrl url(u"qrc:/quickmorphismexample/demo.qml"_qs);
+    //const QUrl url(QStringLiteral("qrc:/Demo.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
         if (!obj && url == objUrl)
